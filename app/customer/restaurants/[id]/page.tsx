0a91f7d2c76305c0d6901ad514d045df00ce5bde
Siyam Talukder <mdsiyamtalukder1@gmail.com>
2026-05-19 08:13:54 +0600

    double

--- app/customer/restaurants/[id]/page.tsx
+++ app/customer/restaurants/[id]/page.tsx
@@ -51,12 +51,6 @@ export default async function RestaurantPage({
 
   return (
     <div className="min-h-screen bg-slate-50">
-      <div className="bg-white border-b border-gray-100">
-        <main className="max-w-6xl xl:mx-auto w-full">
-          <Navbar />
-        </main>
-      </div>
-
       <main className="max-w-6xl xl:mx-auto w-full px-8 py-8">
         <RestaurantDetailsClient restaurant={restaurantData} />
       </main>
